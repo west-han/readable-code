1f40e0fcea5dcc7ec0c23ebccd41ba72ab93f867
Han SeokHyeon <westhan98@gmail.com>
2025-06-03 21:50:18 +0900

    init

---
+++ src/main/java/cleancode/studycafe/mission/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.mission;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/cleancode/studycafe/mission/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.mission.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
