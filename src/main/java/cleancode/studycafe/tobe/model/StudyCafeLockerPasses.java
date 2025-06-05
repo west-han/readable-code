5dbf734ab73a155811683ae07e298b309e0683d9
Han SeokHyeon <westhan98@gmail.com>
2025-06-06 00:47:09 +0900

    일급 컬렉션 적용

---
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafeLockerPasses.java
@@ -0,0 +1,23 @@
+package cleancode.studycafe.tobe.model;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StudyCafeLockerPasses {
+
+    private final List<StudyCafeLockerPass> passes;
+
+    private StudyCafeLockerPasses(List<StudyCafeLockerPass> passes) {
+        this.passes = passes;
+    }
+
+    public static StudyCafeLockerPasses of(List<StudyCafeLockerPass> passes) {
+        return new StudyCafeLockerPasses(passes);
+    }
+
+    public Optional<StudyCafeLockerPass> findLockerPassBy(StudyCafePass pass) {
+        return passes.stream()
+                .filter(pass::isSameDurationType)
+                .findFirst();
+    }
+}
